148101ec689d63bd8ad21c615e79785c1792baf9
Robert Li <v.litvinenko@hotmail.com>
2023-08-03 23:18:27 +0300

    using Chart.js in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyChartComponent } from './chart/my-chart/my-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyChartComponent
   ],
   imports: [
     BrowserModule
